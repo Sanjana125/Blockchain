21ffcb6108937416ec68b761357f179e9eb1911a
Sanjana125 <66018861+Sanjana125@users.noreply.github.com>
2021-07-29 11:41:26 +0530

    Update Election.sol
    Comment line

--- Smart contract/contracts/Election.sol
+++ Smart contract/contracts/Election.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.9.4;
 
 contract Election {
     string public candidateName;
-
+//Read value from client-side application
     function Election () public {
         candidateName = "Candidate 1";
     }
